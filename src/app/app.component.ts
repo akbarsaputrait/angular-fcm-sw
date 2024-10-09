7936f2e5fddf3878347f3a604ced5c4179ff5e0e
akbarsaputrait <akbarsaputrait@outlook.com>
2024-10-09 16:50:17 +0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  getToken,
+  MessagePayload,
+  Messaging,
+  NotificationPayload,
+  onMessage,
+} from '@angular/fire/messaging';
 import { RouterOutlet } from '@angular/router';
+import { NotificationService } from './notification.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private readonly _notification = inject(NotificationService);
+  messages: NotificationPayload[] = [];
+
   title = 'angular-fcm';
+
+  ngOnInit(): void {
+    this._notification.requestPermissions();
+    this._notification.getToken().subscribe((token) => {
+      if (token) {
+        console.log('Notification initialized with token:', token);
+        this._notification.listenForMessages();
+      }
+    });
+
+    this._notification.getMessage().subscribe({
+      next: (message) => {
+        if (message) {
+          console.log('Message received:', message);
+          if (message.notification) {
+            this.messages.push(message.notification);
+          }
+        }
+      },
+      error: (error) => {
+        console.error('Error receiving message:', error);
+      },
+      complete: () => {
+        console.log('Message stream completed');
+      },
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+} from '@angular/fire/analytics';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideMessaging(() => getMessaging()),
+    ScreenTrackingService,
+  ],
 };
